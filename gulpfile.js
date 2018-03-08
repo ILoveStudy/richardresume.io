7f2a7b43319317bab661c540895e206d40d8eb6e
richardgong1987 <909253305@qq.com>
2018-03-08 23:09:33 +0800

    a

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const markdown = require('gulp-markdown');
-
-gulp.task('default', () =>
-gulp.src('english.resume.md')
-    .pipe(markdown())
-    .pipe(gulp.dest('dist'))
-);
--- test.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by richardgong on 1/25/16.
- */
-
-var path = require('path');
-var pa = path.normalize('/foo/bar//baz/asdf/quux/..');
-console.log(pa);
